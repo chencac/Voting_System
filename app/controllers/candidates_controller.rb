b29f3138dda7c75a00375035a4423957abc582ba
chen <zx656084@gmail.com>
2023-03-22 19:34:52 +0800

    Voting_System n1

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,61 @@
+class CandidatesController < ApplicationController
+
+    def index
+        @candidates = Candidate.all
+    end
+    def show
+        @candidate = Candidate.find_by(id: params[:id])
+    end
+    def edit
+        @candidate = Candidate.find_by(id: params[:id])
+    end
+
+    def new
+       @candidate = Candidate.new
+    end
+
+    def create
+       
+        @candidate = Candidate.new(candidate_params)
+
+        if @candidate.save
+          flash[:notice] = "Candidate created!"
+          redirect_to '/candidates'
+        else
+          render :new    
+        end    
+    end
+    def update
+        @candidate = Candidate.find_by(id: params[:id])
+
+        if @candidate.update(candidate_params)
+            flash[:notice] = "Candidate created!"
+            redirect_to '/candidates'
+        else
+            render :edit    
+        end  
+    end
+    def destroy
+        @candidate = Candidate.find_by(id: params[:id])
+        @candidate.destroy
+
+          flash[:notice] = "Candidate deleted!"
+          redirect_to '/candidates'
+    end
+
+    def vote
+        @candidate = Candidate.find_by(id: params[:id])
+        @candidate.increment(:votes)
+        @candidate.save
+        
+        flash[:notice] = "vote!"
+        redirect_to '/candidates'
+    end
+
+
+    private
+    def candidate_params
+    clean_params =params.require(:candidate).permit(:name, :party,:age,:politics)
+    end
+
+end
